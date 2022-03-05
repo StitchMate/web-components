96f3717ea2f3fc62bcea7e94365f1c1506e55fde
Nicholas Frush <cawfeecoder@Nicholass-MacBook-Pro.local>
2022-03-05 14:37:41 -0500

    formatting

--- src/seam-button/seam-button.tsx
+++ src/seam-button/seam-button.tsx
@@ -1,97 +1,139 @@
-import { c, useEffect, useEvent, Props } from "atomico";
+import { c, Props, useEffect, useEvent } from "atomico";
 import classNames from "classnames";
-import styles from "./seam-button.css";
 import tailwindcss from "../tailwindcss.css";
+import styles from "./seam-button.css";
 
-function seamButton({ loading, success, fail, variant, spacing, href, target, download }: Props<typeof seamButton>) {
-    let loadingDispatch = useEvent("seam-loading", {
-        bubbles: true,
-        composed: true
-    });
+function seamButton({
+  loading,
+  success,
+  fail,
+  variant,
+  spacing,
+  href,
+  target,
+  download,
+}: Props<typeof seamButton>) {
+  let loadingDispatch = useEvent("seam-loading", {
+    bubbles: true,
+    composed: true,
+  });
 
-    let successDispatch = useEvent("seam-success", {
-        bubbles: true,
-        composed: true
-    });
+  let successDispatch = useEvent("seam-success", {
+    bubbles: true,
+    composed: true,
+  });
 
-    let failDispatch = useEvent("seam-fail", {
-        bubbles: true,
-        composed: true
-    });
+  let failDispatch = useEvent("seam-fail", {
+    bubbles: true,
+    composed: true,
+  });
 
-    let clickDispatch = useEvent("seam-click", {
-        bubbles: true,
-        composed: true
-    });
+  let clickDispatch = useEvent("seam-click", {
+    bubbles: true,
+    composed: true,
+  });
 
-    useEffect(() => {
-        loading ? loadingDispatch() : null;
-        success ? successDispatch() : null;
-        fail ? failDispatch() : null;
-    });
+  useEffect(() => {
+    loading ? loadingDispatch() : null;
+    success ? successDispatch() : null;
+    fail ? failDispatch() : null;
+  });
 
-    return (
-        <host shadowDom>
-            {variant == "link" ?
-                (<a part="base" href={href} target={target} download={download} onclick={() => clickDispatch()} class={classNames("button", {
-                    "button--link": variant == "link",
-                    "button-spacing--default": spacing == "default",
-                    "button-spacing--compact": spacing == "compact"
-                })}>
-                    {loading ? <seam-loading-icon></seam-loading-icon> : success ? <seam-check-icon></seam-check-icon> : fail ? <seam-x-icon></seam-x-icon> : <span part="label"><slot></slot></span>}
-                </a>) :
-                (<button part="base" onclick={() => clickDispatch()} class={classNames("button", {
-                    "button--loading": loading && variant != "link",
-                    "button--success": success && variant != "subtle",
-                    "button--success--subtle": success && variant == "subtle",
-                    "button--fail": fail && variant != "subtle",
-                    "button--fail--subtle": fail && variant == "subtle",
-                    "button--primary": !success && !fail && variant == "primary",
-                    "button--subtle": !success && !fail && variant == "subtle",
-                    "button-spacing--default": spacing == "default",
-                    "button-spacing--compact": spacing == "compact"
-                })}>
-                    {loading ? <seam-loading-icon></seam-loading-icon> : success ? <seam-check-icon></seam-check-icon> : fail ? <seam-x-icon></seam-x-icon> : <span part="label"><slot></slot></span>}
-                </button>)}
-        </host>
-    );
+  return (
+    <host shadowDom>
+      {variant == "link" ? (
+        <a
+          part="base"
+          href={href}
+          target={target}
+          download={download}
+          onclick={() => clickDispatch()}
+          class={classNames("button", {
+            "button--link": variant == "link",
+            "button-spacing--default": spacing == "default",
+            "button-spacing--compact": spacing == "compact",
+          })}
+        >
+          {loading ? (
+            <seam-loading-icon></seam-loading-icon>
+          ) : success ? (
+            <seam-check-icon></seam-check-icon>
+          ) : fail ? (
+            <seam-x-icon></seam-x-icon>
+          ) : (
+            <span part="label">
+              <slot></slot>
+            </span>
+          )}
+        </a>
+      ) : (
+        <button
+          part="base"
+          onclick={() => clickDispatch()}
+          class={classNames("button", {
+            "button--loading": loading && variant != "link",
+            "button--success": success && variant != "subtle",
+            "button--success--subtle": success && variant == "subtle",
+            "button--fail": fail && variant != "subtle",
+            "button--fail--subtle": fail && variant == "subtle",
+            "button--primary": !success && !fail && variant == "primary",
+            "button--subtle": !success && !fail && variant == "subtle",
+            "button-spacing--default": spacing == "default",
+            "button-spacing--compact": spacing == "compact",
+          })}
+        >
+          {loading ? (
+            <seam-loading-icon></seam-loading-icon>
+          ) : success ? (
+            <seam-check-icon></seam-check-icon>
+          ) : fail ? (
+            <seam-x-icon></seam-x-icon>
+          ) : (
+            <span part="label">
+              <slot></slot>
+            </span>
+          )}
+        </button>
+      )}
+    </host>
+  );
 }
 
 seamButton.props = {
-    variant: {
-        type: String,
-        value: "primary",
-        reflect: true
-    },
-    loading: {
-        type: Boolean,
-        value: false,
-        reflect: true
-    },
-    success: {
-        type: Boolean,
-        value: false,
-        reflect: true
-    },
-    fail: {
-        type: Boolean,
-        value: false,
-        reflect: true
-    },
-    spacing: {
-        type: String,
-        value: "default",
-        reflect: true
-    },
-    href: {
-        type: String,
-    },
-    target: {
-        type: String
-    },
-    download: {
-        type: String
-    }
+  variant: {
+    type: String,
+    value: "primary",
+    reflect: true,
+  },
+  loading: {
+    type: Boolean,
+    value: false,
+    reflect: true,
+  },
+  success: {
+    type: Boolean,
+    value: false,
+    reflect: true,
+  },
+  fail: {
+    type: Boolean,
+    value: false,
+    reflect: true,
+  },
+  spacing: {
+    type: String,
+    value: "default",
+    reflect: true,
+  },
+  href: {
+    type: String,
+  },
+  target: {
+    type: String,
+  },
+  download: {
+    type: String,
+  },
 };
 
 seamButton.styles = [styles, tailwindcss];
--- src/seam-check-icon/seam-check-icon.jsx
+++ src/seam-check-icon/seam-check-icon.jsx
@@ -1,15 +1,27 @@
-import { c, css } from "atomico";
+import { c } from "atomico";
 
 function seamCheckIcon() {
-    return (
-        <host shadowDom>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
-        </host>
-    );
+  return (
+    <host shadowDom>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-check"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
+    </host>
+  );
 }
 
-seamCheckIcon.props = {
-};
+seamCheckIcon.props = {};
 
 seamCheckIcon.styles = [];
 
